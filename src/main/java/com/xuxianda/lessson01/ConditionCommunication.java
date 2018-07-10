1280a68165542e3e1cf2d669125e9b6ac17b30bd
XuXianda <1114186632@qq.com>
2018-07-10 09:03:52 +0800

    condition  await signal替代 wait,notify

---
+++ src/main/java/com/xuxianda/lessson01/ConditionCommunication.java
@@ -0,0 +1,71 @@
+package com.xuxianda.lessson01;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Xianda Xu on 2018/07/06 17:29.
+ */
+public class ConditionCommunication {
+
+    public static void main(String[] args) {
+
+        final Business Business = new Business();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 50; i++) {
+                    Business.sub(i);
+                }
+            }
+        }).start();
+        for (int i = 0; i < 50; i++) {
+            Business.main(i);
+        }
+
+    }
+
+    static class Business{
+
+        boolean shouldSub = true;
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        public void sub(int i){
+            lock.lock();
+            while(!shouldSub){
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            for (int j = 0; j < 10; j++) {
+                System.out.println("sub thread " + j + ", loop" + i);
+            }
+            shouldSub = false;
+            condition.signal();
+            lock.unlock();
+        }
+
+        public void main(int i){
+            lock.lock();
+            while(shouldSub){
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            for (int j = 0; j < 10; j++) {
+                System.out.println("main thread " + j + ", loop" + i);
+            }
+            shouldSub = true;
+            condition.signal();
+            lock.unlock();
+        }
+
+    }
+
+}
+
